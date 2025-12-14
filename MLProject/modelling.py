a03738b74a40f7d3d97429cc93a94f8bd82fbdf0
Farhan Ainurrahman <farhanainurr@student.ub.ac.id>
2025-12-15 06:28:44 +0700

    fix mdelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,12 +4,9 @@ import mlflow.sklearn
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-# Load data preprocessing
+# Load data
 data = pd.read_csv("bankdataset_preprocessing.csv")
 X = data.values
-
-# Set experiment (aman, tidak bikin konflik)
-mlflow.set_experiment("CI_KMeans_Experiment")
 
 # Train model
 kmeans = KMeans(n_clusters=3, random_state=42)
@@ -18,11 +15,11 @@ kmeans.fit(X)
 labels = kmeans.labels_
 score = silhouette_score(X, labels)
 
-# Manual logging (WAJIB untuk skilled/advanced)
+# Manual logging
 mlflow.log_param("n_clusters", 3)
 mlflow.log_metric("silhouette_score", score)
 
-# Log model artifact
+# Log model
 mlflow.sklearn.log_model(kmeans, "kmeans_model")
 
 print("Training completed")
